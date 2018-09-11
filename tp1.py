8580e9eb2458074d7d7a7260d2217bb72e085701
agustin <zurettiagustin@gmail.com>
2018-09-10 21:51:29 -0300

    cantidad de eventos por dia del mes

--- tp1.py
+++ tp1.py
@@ -16,26 +16,18 @@ def preprocesado1(eventos):
 	evs = eventos.iloc[:,:8]
 	evs = evs.drop(columns=['url'])
 	evs['fecha'] = pd.to_datetime(evs.timestamp, format = '%Y-%m-%d %H:%M:%S') 
-	evs['dia'] = evs['fecha'].apply(lambda x: x.weekday)	
+	evs['dia'] = evs['fecha'].apply(lambda x: x.day)	
 	evs['mes'] = evs['fecha'].apply(lambda x: x.month)
 	evs['anio'] = evs['fecha'].apply(lambda x: x.year)
 	evs['cantidad']= 1
 	return evs
 
-def eventosPorDiaDeLaSemana(eventos):
-#	nuevo = pd.DataFrame({'cantidad': dat.groupby('hermano')['viajes'].sum()}).reset_index()
-#	evs = pd.DataFrame({'cantidad': eventos.groupby('dia')['cantidad'].sum()}).reset_index()
-#	x = ['lunes','martes','miercoles','jueves','viernes']
-#	plt.bar(range(len(x), evs.))
-#	plt.xticks(range(len(x)),x)
-	eventos.groupby('dia').sum().plot(figsize=(14,4))
+def eventosPorDiaDelMes(eventos):
+	eventos['dia'].value_counts().sort_index().plot(kind = 'bar',figsize=(14,4))
 	plt.show()
 
 def main():
 	eventos = pd.read_csv('events.csv', low_memory=False)
 	eventos1 = preprocesado1(eventos)
-	#print(eventos1.head(3))
-	#print(eventos1.info())
-	eventosPorDiaDeLaSemana(eventos1)
-
+	eventosPorDiaDelMes(eventos1)
 main()
